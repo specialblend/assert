0974c5cd858ff1018749334ff36bcb8b79889ced
specialblend <cj@specialblend.net>
2019-07-03 18:09:17 -0500

    remove second line (assertTrace) from exception stacktrace

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const R = require('ramda');
 const assert = require('assert');
 const util = require('util');
 
-const { join, insert, split, map, compose, converge, always, tap, tryCatch, path, concat, curry, prop } = R;
+const { always, compose, concat, converge, curry, join, map, path, prop, split, tryCatch, update } = R;
 
 /**
  * Reconstruct caller function arguments
@@ -49,9 +49,8 @@ const injectCaller = curry(
     (message, stack) =>
         compose(
             join('\n'),
-            insert(1, formatStackTraceLine(message)),
-            tap(console.log),
-            split('\n')
+            update(1, formatStackTraceLine(message)),
+            split('\n'),
         )(stack)
 );
 
